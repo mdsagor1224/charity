ca6972e34631f516ad5ea46184abe34932c0b8f3
mdsagor <mdsagorahmed1224@gmail.com>
2024-02-09 07:48:51 +0600

    class#2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
+
+    // mangify popup
     $('.popup-video').magnificPopup({
         type:'iframe'
     });
+
+    //testimonial
+    $('.testimonial-active').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
   });
